4e7a9d7b1bd3da3ff262fd85368cd9b3a6150ff3
Austin Herman <austin_herman@jabil.com>
2022-06-01 16:34:44 -0400

    removed unecessary datacore header

--- byteknight/datacore/datacore.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef CORE_DATA_HPP
-#define CORE_DATA_HPP
-
-#include "object_core.hpp"
-#include "common_core.hpp"
-#include "widget_core.hpp"
-#include "event_core.hpp"
-
-#endif // CORE_DATA_HPP
